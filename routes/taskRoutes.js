a8af35cefa0b18da9600950d01e8828b6f0b31f0
Berk Gülay <berkgulay@berks-mbp-2.mynetworksettings.com>
2023-04-26 16:24:57 -0400

    Finished up employee and task routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -10,6 +10,24 @@ router.get('/', ash(async(req, res) => {
     let tasks = await Task.findAll({include: [Employee]});
     res.status(200).json(tasks);
   }));
+router.get('/:id', ash(async(req, res) => {
+    let task = await Task.findByPk(req.params.id,{include: [Employee]});
+    res.status(200).json(task);
+  }));
+router.post('/', ash(async(req, res) => {
+    let task = await Task.create(req.body);
+    res.status(200).json(task);
+  }));
+router.put('/:id', ash(async(req, res) => {
+    let task = await Task.update(req.body, {where: {id:req.params.id}});
+    res.status(200).json({message:"Task updated"});
+  }));
+
+router.delete('/:id', ash(async(req, res) => {
+  let task = await Task.destroy({where: {id:req.params.id}});
+  res.status(200).json({message:"Task deleted"});
+}));
+
 
 
 
